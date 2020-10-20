83f368a1766972ae8be98a12c1a16f39968a739b
QSD_o <info@qsdbih.com>
2020-10-20 16:29:35 +0200

    Country list UI changes, Version bump

--- Covid19VirusTracker/View/CountryCovidView.swift
+++ Covid19VirusTracker/View/CountryCovidView.swift
@@ -53,35 +53,45 @@ struct CountryCovidView: View {
             }
             
             List(covidStats.Countries, id: \.self) { countryStat in
-                HStack(alignment: .center, spacing: 10) {
-                    Text(countryStat.Country)
-                        .bold()
-                        .foregroundColor(.black)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15.0, style: .continuous)
+                        .fill(Color.white)
+                        .shadow(radius: 10.0)
                     
-                    Spacer()
-                    
-                    HStack {
-                        Text("\(countryStat.TotalConfirmed)")
-                            .bold()
-                            .foregroundColor(.orange)
-                            .minimumScaleFactor(0.5)
+                    VStack(alignment: .center) {
+                        HStack {
+                            Spacer()
+                            Text(countryStat.Country)
+                                .bold()
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
                         
-                        Text("\(countryStat.TotalDeaths)")
-                            .bold()
-                            .foregroundColor(.red)
-                            .minimumScaleFactor(0.5)
-                        
-                        Text("\(countryStat.TotalRecovered)")
-                            .bold()
-                            .foregroundColor(.green)
-                            .minimumScaleFactor(0.5)
+                        HStack(alignment: .center) {
+                            Text("\(countryStat.TotalConfirmed)")
+                                .bold()
+                                .foregroundColor(.orange)
+                                .minimumScaleFactor(0.5)
+                            
+                            Text("\(countryStat.TotalDeaths)")
+                                .bold()
+                                .foregroundColor(.red)
+                                .minimumScaleFactor(0.5)
+                            
+                            Text("\(countryStat.TotalRecovered)")
+                                .bold()
+                                .foregroundColor(.green)
+                                .minimumScaleFactor(0.5)
+                        }
                     }
-                    .padding(.leading, 10)
+                    .padding([.top, .bottom], 6.0)
                 }
+                .padding([.top, .bottom], 10.0)
             }
             .onAppear {
                 getCovidStats()
             }
+            
         }
         .navigationTitle("Global stats")
     }
